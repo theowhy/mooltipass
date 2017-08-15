d7cf2a78aef5081f1641a602e113ebb4f6a9cd29
Theowhy <theowhy@shizoku.fr>
2017-08-15 11:27:31 +0200

    Remove chrome application

--- chrome_app/html/js/seedrandom_after.js
+++
@@ -1,3 +0,0 @@
-// These commands has to be executed right after seedrandom.js is loaded
-// It's in an own file because inline-scripting is not allowed in a Chrome app
-Math.seedrandom(initial_random_value, { entropy: true });
--- chrome_app/html/js/seedrandom_before.js
+++
@@ -1,3 +0,0 @@
-// These commands has to be executed before seedrandom.js is loaded
-// It's in an own file because inline-scripting is not allowed in a Chrome app
-var initial_random_value = Math.random();
